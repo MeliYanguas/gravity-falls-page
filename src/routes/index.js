0a74dc1dbbb307a1d33eba8da7a4432e934bd68d
melina yanguas <melinayanguas@gmail.com>
2021-04-10 21:47:21 -0300

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.html', { title: 'Node First Website'});
+    res.render('index.html', { title: 'Gravity Falls'});
     
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact.html', { title: 'Contact Page'});
+    res.render('contact.html', { title: 'Contacto'});
+    
+});
+
+router.get('/video1', (req, res) => {
+    res.render('video1.html', { title: 'Video misterioso'});
     
 });
 
